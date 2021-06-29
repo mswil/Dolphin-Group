ffabbfcba7637be8f0e819f110998965d783fbea
Joshua D Wood <joshuawd190@gmail.com>
2021-06-29 17:08:51 -0500

    Dummy routes and page updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,47 +3,43 @@ const sequelize = require('../config/connection');
 const { Item, Category } = require('../models');
 
 const findItems = async (itemName, categoryId) => {
-    const include = {
-        model: Category,
-        attributes: [['name', 'category_name']]
-    }
+  const include = {
+    model: Category,
+    attributes: [['name', 'category_name']],
+  };
 
-    const where = {}
-    if (itemName) {
-        where.name = itemName;
-    }
-    if (categoryId) {
-        where.category_id = parseInt(categoryId);
-    }
+  const where = {};
+  if (itemName) {
+    where.name = itemName;
+  }
+  if (categoryId) {
+    where.category_id = parseInt(categoryId);
+  }
 
-    return await Item.findAll({ where, include });
-
-}
+  return await Item.findAll({ where, include });
+};
 
 const findCategories = async () => {
-
-    return await Category.findAll({});
-}
+  return await Category.findAll({});
+};
 
 router.get('/', async (req, res) => {
-    
-    try {
-        // http://localhost:3001/?item_name=name
-        const items = await findItems(req.query.item_name, req.query.category_id);
-        // http://localhost:3001/?category_id=#
-        const categories = await findCategories();
-         res.render('homepage', {
-             items,
-             categories,
-             loggedIn: req.session.loggedIn,
-             isAdmin: req.session.isAdmin
-         })
-    }
-    catch (err) {
-        res.status(500).json(err)
-    }
+  try {
+    // http://localhost:3001/?item_name=name
+    const items = await findItems(req.query.item_name, req.query.category_id);
+    // http://localhost:3001/?category_id=#
+    const categories = await findCategories();
+    const itemPlain = items.map((data) => data.get({ plain: true }));
+    console.log(itemPlain);
+    res.render('homepage', {
+      itemPlain,
+      categories,
+      loggedIn: req.session.loggedIn,
+      isAdmin: req.session.isAdmin,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,15 @@ const router = require('express').Router();
 const { Item } = require('../models');
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const adminRoutes = require('./admin-routes');
+const cartRoutes = require('./cart-routes');
+const loginRoutes = require('./login-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/admin', adminRoutes);
+router.use('/cart', cartRoutes);
+router.use('/login', loginRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
