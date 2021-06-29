e89461a9ae7eaa7815a7c0ebd91440ec172cdcfd
Molly Wilson <molly_wilson1@outlook.com>
2021-06-28 20:06:04 -0500

    Item api endpoints, GET all, GET by Id, POST, PUT, and DELETE items

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const orderRoutes = require('./order-routes');
+const itemRoutes = require('./item-routes');
 
 router.use('/users', userRoutes);
 router.use('/orders', orderRoutes);
+router.use('/items', itemRoutes);
 
 module.exports = router;
