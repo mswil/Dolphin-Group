9108949fc6a597e4b8c60147604a0b144ea04eae
Molly Wilson <molly_wilson1@outlook.com>
2021-06-29 16:56:49 -0500

    login and logout endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const loginRoutes = require('./login-routes');
 
 router.use('/api', apiRoutes);
+router.use('/user', loginRoutes);
 router.use('/', homeRoutes);
 
 router.use((req, res) => {
