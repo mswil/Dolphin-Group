e46eaad283477684712fd563b9ed919c526c67d0
Molly Wilson <molly_wilson1@outlook.com>
2021-06-29 13:42:31 -0500

    get items and categories for homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Item, Category } = require('../models');
+
+const findItems = async itemName => {
+
+    const include = {
+        model: Category,
+        attributes: [['name', 'category_name']]
+    }
+
+    if (itemName) {
+        const items = await Item.findAll({
+            where: {
+                name: itemName
+            },
+            include
+        })
+        if (items.length === 0) {
+            throw { message: `No match for ${itemName}` }
+        }
+        return items;
+    }
+    else {
+        const items = await Item.findAll({ include });
+        return items;
+    }
+}
+
+const findCategories = async categoryName => {
+    const include = {
+        module: Item
+    }
+
+    if(categoryName) {
+        const categories = await Category.findAll({
+            where: {
+                name: categoryName
+            },
+            include
+        })
+        if (categories.length === 0) {
+            throw { message: `No category called ${categoryName}`}
+        }
+        return categories;
+    }
+    else {
+        const categories = await Category.findAll({include});
+        return categories;
+    }
+}
+
+router.get('/', (req, res) => {
+
+    try {
+        const items = await findItems();
+        const categories = await findCategories();
+        res.render('homepage', {items, categories})
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
