b42a6cfc8d53ce6e7c8f7d892154b435e21243b0
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-06-30 10:50:37 -0500

    added sign up page and logout button on home page

--- controllers/login-routes.js
+++ controllers/login-routes.js
@@ -53,4 +53,14 @@ router.post('/logout', withAuth, (req, res) => {
   }
 });
 
+//sign up page
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/')
+    return
+  }
+
+  res.render('signup')
+})
+
 module.exports = router;
